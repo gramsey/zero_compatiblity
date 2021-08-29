c72e9ab800ff067d4dcf4376975b90c304275d83
graham <graham@ulsterit.com>
2021-08-29 15:09:44 +0100

    added can_interact_with_player

--- default/misc.lua
+++ default/misc.lua
@@ -1,1 +1,9 @@
 default.LIGHT_MAX = minetest.LIGHT_MAX
+
+function default.can_interact_with_node(player, pos)
+	local name = player:get_player_name()
+	local protected = minetest.is_protected(pos, name) 
+	local locked = lock and lock.is_locked(pos, player)
+
+	return not (protected or locked)
+end
---
+++ stairs/init.lua
@@ -0,0 +1,5 @@
+
+stairs = { shim = {}} 
+
+local mod_path = minetest.get_modpath("stairs")
+dofile(mod_path.."/stairs.lua")
